b22d4cb143e7398328de9173caad9d471c1c7e88
Sam Ruby <rubys@intertwingly.net>
2009-12-02 10:45:08 -0500

    Initial support for omitting sections

--- lib/gorp.rb
+++ lib/gorp.rb
@@ -13,6 +13,17 @@ require 'gorp/env'
 require 'rbconfig'
 $ruby = File.join(Config::CONFIG["bindir"], Config::CONFIG["RUBY_INSTALL_NAME"])
 
+# indicate that a given step should be omitted
+def omit *sections
+  sections.each do |section|
+    STDERR.puts section.inspect
+    section = [section] unless section.respond_to? :include?
+    STDERR.puts section.inspect
+    $omit << Range.new(secsplit(section.first), secsplit(section.last))
+  end
+  STDERR.puts $omit.inspect
+end
+
 # Micro DSL for declaring an ordered set of book sections
 $sections = []
 def section number, title, &steps
@@ -31,6 +42,7 @@ $toc = Builder::XmlMarkup.new(:indent => 2)
 $todos = Builder::XmlMarkup.new(:indent => 2)
 $issue = 0
 $style = Builder::XmlMarkup.new(:indent => 2)
+$omit  = []
 
 FileUtils.mkdir_p $WORK
 
@@ -562,6 +574,14 @@ def secsplit section
   section.to_s.split('.').map {|n| n.to_i}
 end
 
+def secinclude ranges, section
+  # was (in Ruby 1.8): range.include?(secsplit(section))
+  ranges.any? do |range| 
+    ss = secsplit(section)
+    (range.first <=> ss) <= 0 and (range.last <=> ss) >= 0
+  end
+end
+
 at_exit do
   $x.html :xmlns => 'http://www.w3.org/1999/xhtml' do
     $x.header do
@@ -609,7 +629,7 @@ at_exit do
         bounds = arg.split(/-|\.\./)
         Range.new(secsplit(bounds.first), secsplit(bounds.last))
       end
-      ARGV.push 'partial' unless ranges.empty?
+      ARGV.push 'partial' unless ranges.empty? and $omit.empty?
   
       # optionally save a snapshot
       if ARGV.include? 'restore'
@@ -628,12 +648,8 @@ at_exit do
       e = nil
       begin
         $sections.each do |section, title, steps|
-	  next if !ranges.empty? and 
-                  !ranges.any? do |range| 
-                    # was (in Ruby 1.8): range.include?(secsplit(section))
-                    ss = secsplit(section)
-                    (range.first <=> ss) <= 0 and (range.last <=> ss) >= 0
-                  end
+	  next if !ranges.empty? and !secinclude(ranges, section)
+	  next if secinclude($omit, section)
 	  head section, title
 	  steps.call
         end
