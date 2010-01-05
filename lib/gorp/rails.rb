854fd8edbefa1ef95eecba256fef28ead1280fc2
Sam Ruby <rubys@intertwingly.net>
2010-01-05 11:46:17 -0500

    More calls to to_s required

--- lib/gorp/rails.rb
+++ lib/gorp/rails.rb
@@ -13,7 +13,7 @@ end
 # select a version of Rails
 if ARGV.first =~ /^_\d[.\d]*_$/
   $rails = "rails #{ARGV.first}"
-elsif File.directory?(ARGV.first.split(File::PATH_SEPARATOR).to_s.first)
+elsif File.directory?(ARGV.first.to_s.split(File::PATH_SEPARATOR).first.to_s)
   if ARGV.first.include?(File::PATH_SEPARATOR)
     # first path is Rails, additional paths are added to the RUBYLIBS
     libs = ENV['RUBYLIBS'].to_s.split(File::PATH_SEPARATOR)
