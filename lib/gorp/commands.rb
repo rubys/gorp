e5140a9cc8d5300c694304f2c5be9d2f37e562a8
Sam Ruby <rubys@intertwingly.net>
2010-11-14 09:03:34 -0500

    Move date

--- lib/gorp/commands.rb
+++ lib/gorp/commands.rb
@@ -175,8 +175,8 @@ module Gorp
 
       if args == 'rake db:migrate'
 	Dir.chdir 'db/migrate' do
-	  date = '20100301000000'
-	  Dir['[0-9]*'].sort_by {|fn| fn=~/201003/ ? fn : 'x'+fn}.each do |file|
+	  date = '20110211000000'
+	  Dir['[0-9]*'].sort_by {|fn| fn=~/201102/ ? fn : 'x'+fn}.each do |file|
 	    file =~ /^([0-9]*)_(.*)$/
 	    FileUtils.mv file, "#{date}_#{$2}" unless $1 == date.next!
 	    $x.pre "mv #{file} #{date}_#{$2}"  unless $1 == date
