c39585f10e5a85c26af2b8a2dff84221e559d24a
Sam Ruby <rubys@intertwingly.net>
2011-07-28 21:33:21 -0400

    Make sure that the necessary preconditions are met before checking for a Gemfile

--- lib/gorp/rails.rb
+++ lib/gorp/rails.rb
@@ -61,7 +61,7 @@ end
 if $rails =~ /^rails( |$)/
   `#{$rails} -v 2>#{DEV_NULL}`
 else
-  unless ENV['BUNDLE_GEMFILE']
+  if ENV['GORP_HOME'] and ENV['GORP_WORK'] and not ENV['BUNDLE_GEMFILE']
     Dir.chdir File.join(ENV['GORP_HOME'], ENV['GORP_WORK']) do
       require 'bundler/setup' if File.exist? 'Gemfile'
     end
