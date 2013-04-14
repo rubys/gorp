641ca2498a836eb5f0d977bc683f8014c9f6aeb0
Sam Ruby <rubys@intertwingly.net>
2013-04-14 19:31:34 -0400

    properly clean up bundle environment

--- lib/gorp/rails.rb
+++ lib/gorp/rails.rb
@@ -169,12 +169,7 @@ module Gorp
           end
 
           if $bundle
-            begin
-              rubyopt, ENV['RUBYOPT'] = ENV['RUBYOPT'], nil
-              cmd "bundle install"
-            ensure
-              ENV['RUBYOPT'] = rubyopt
-            end
+            bundle 'install'
           else
             cmd "ln -s #{$rails} vendor/rails"
             system "mkdir -p .bundle"
